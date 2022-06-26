1259cb0f8904a3035a83d61cd2c3fc2c9cb925d5
Vinay Sharma <sevinaysharma>
2022-06-26 18:02:02 +0530

    Initial Commit

---
+++ AnimatedGradient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedGradient
+//
+//  Created by Vinay Sharma on 26/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
